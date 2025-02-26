4555fe880c00239822e48861b9617ade34dcf8ca
fintihlupik <aminaalive@gmail.com>
2025-02-26 18:43:15 +0100

    feat(settings): update database configuration

--- app_trucks/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppTrucksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_trucks'
--- project_trucks/settings.py
+++ project_trucks/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_trucks',
+    'app_companies',
+    'app_users',
     'rest_framework',
 ]
 
@@ -77,8 +82,12 @@ WSGI_APPLICATION = 'project_trucks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('dbname'),
+        'USER': os.getenv('user'),
+        'PASSWORD': os.getenv('password'),
+        'HOST': os.getenv('host'),
+        'PORT': os.getenv('port'),
     }
 }
 

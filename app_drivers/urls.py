287070d42fcfe733e453f5f60c2975742f2055df
César_Mercado <doncesarmercado@hotmail.com>
2025-03-03 19:42:03 +0100

    Fix(app_drivers): urls.py

--- app_drivers/urls.py
+++ app_drivers/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import get_all_drivers, create_driver, driver_detail
 
 urlpatterns = [
-    path('drivers/', get_all_drivers, name="get_all_drivers"),
-    path('drivers/create/', create_driver, name="create_driver"),
-    path('drivers/<int:id>/', driver_detail, name="driver_detail"),
+    path("", get_all_drivers, name="get_all_drivers"),
+    path("create/", create_driver, name="create_driver"),
+    path("<int:id>/detail/", driver_detail, name="driver_detail"),
+    path ("<int:id>/update/", driver_detail, name="driver_detail"),
+    path ("<int:id>/delete/", driver_detail, name="driver_detail"),
 ]

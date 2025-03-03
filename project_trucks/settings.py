c58a6c513f9f69399c77f911e81f3c2a8fb9f1a2
César_Mercado <doncesarmercado@hotmail.com>
2025-03-03 19:17:13 +0100

    chore(project_trucks): add settings, urls and views and home.html

--- project_trucks/settings.py
+++ project_trucks/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'project_trucks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "project_trucks/templates")],  # 🔹 Ruta de templates,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

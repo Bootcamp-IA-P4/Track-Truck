e3be7e7ca37bcd7c2914c8feffa897ec8613df75
César_Mercado <doncesarmercado@hotmail.com>
2025-03-03 17:54:04 +0100

    feat(project_trucks): add drivers url path

--- project_trucks/urls.py
+++ project_trucks/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('app_users.urls')),
     path('companies/', include('app_companies.urls')),
+    path('drivers/', include('app_drivers.urls')),
 ]

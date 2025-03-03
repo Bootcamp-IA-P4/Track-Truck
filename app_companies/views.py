12a3b5e1e71f951095a7aaf4d718554416b6dec4
allharut <alla-pdg@hotmail.es>
2025-03-03 09:15:43 +0100

    feat(dashboard.html, update_company.html): add and style

--- app_companies/views.py
+++ app_companies/views.py
@@ -42,3 +42,53 @@ def companyDetail(request, id):
     elif request.method == 'DELETE':
         company.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+
+
+# VISTAS QUE LLAMAN A LA API Y DEVUELVEN HTMLS
+import requests
+from django.shortcuts import redirect, get_object_or_404
+
+def company_dashboard(request, id):
+    api_url = f"http://127.0.0.1:8000/companies/{id}/detail/"  # URL de la API
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        company = response.json()
+    else:
+        company = None  # si la API falla, devolvemos None
+
+    return render(request, 'app_companies/dashboard.html', {'company': company})
+
+def update_company(request, id):
+    api_url = f"http://127.0.0.1:8000/companies/{id}/detail/"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        company = response.json()
+    else:
+        return render(request, "app_companies/update_company.html", {"error": "Error al obtener datos."})
+
+    if request.method == "POST":
+        data = {
+            "name": request.POST["name"],
+            "email": request.POST["email"],
+            "phone": request.POST["phone"],
+            "address": request.POST["address"],
+            "user_id": company["user_id"],
+        }
+
+        # hacemos la petición PUT a la API para actualizar los datos
+        update_url = f"http://127.0.0.1:8000/companies/{id}/update/"
+        update_response = requests.put(update_url, data=data)
+
+        if update_response.status_code == 200:
+            return redirect(f"/companies/{id}/dashboard/")  # Redirige de vuelta al dashboard
+        else:
+            return render(request, "app_companies/update_company.html", {
+                "company": company,
+                "error": "Error al actualizar la empresa. Verifica los datos."
+            })
+
+    return render(request, "app_companies/update_company.html", {"company": company})

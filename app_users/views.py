4740c2cbd1a9b2c90eb90ea6307f9305bd52798d
Fernando García Catalán <fernandogcatalan@gmail.com>
2025-03-03 09:23:43 +0100

    feat(app_users) : first commit

--- app_users/views.py
+++ app_users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Create your views here.
+def signin(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save()
+            login(request, usuario)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'usuarios/registro.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            usuario = form.get_user()
+            login(request, usuario)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'usuarios/login.html', {'form': form})
+
+def logout(request):
+    logout(request)
+    return redirect('login')

34ba7b84b5234ba4fab52fb533eaa831cb8fd1f5
Fernando García Catalán <fernandogcatalan@gmail.com>
2025-03-04 14:38:56 +0100

    fix(app_shipments) : id removed from admin.py list_display

--- app_shipments/models.py
+++ app_shipments/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Shipment(models.Model):
-    id = models.IntegerField(unique=True,null=False,primary_key=True)
-    company_id = models.IntegerField(unique=True,null=False)
-    driver_id = models.IntegerField(unique=True,null=False)
+    #id = models.IntegerField(unique=True,primary_key=True)
+    company_id = models.IntegerField(null=False)
+    driver_id = models.IntegerField(null=False)
     description = models.TextField()
     origin = models.TextField()
     destination = models.TextField()

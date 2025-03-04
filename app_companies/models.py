e0a38148f48758500a70bf6ad8ceb8bda87dd7d9
fintihlupik <aminaalive@gmail.com>
2025-03-04 12:31:05 +0100

    feat(app_companies): add admin.py and update models.py

--- app_companies/models.py
+++ app_companies/models.py
@@ -6,7 +6,7 @@ class Company(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
-    address = models.CharField(max_length=100,null=True)
+    address = models.CharField(max_length=100,null=True,blank=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='companies', db_column='user_id')
 
     class Meta:

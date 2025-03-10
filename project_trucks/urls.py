a067b6bfca0924633413b9d023dd923be67c6519
Fernando García Catalán <fernandogcatalan@gmail.com>
2025-03-10 12:54:42 +0100

    fix(project) : urls.py removed 'path' import at line 19

--- project_trucks/urls.py
+++ project_trucks/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, re_path
+from .views import home
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 

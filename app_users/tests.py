d110d1d2cdcf27a08dd977a80a220d8b51648199
César_Mercado <doncesarmercado@hotmail.com>
2025-03-10 14:51:51 +0100

    test(app_users): add test_signin

--- app_users/tests.py
+++ app_users/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import User
 
-# Create your tests here.
+class UserAPITestCase(TestCase):
+    def setUp(self):
+        """Configura los datos iniciales para los tests."""
+        self.client = APIClient()
+        self.user_data = {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'password': 'TestPass123!',
+            'user_type': 'driver'
+        }
+        self.user = User.objects.create_user(**self.user_data)
+    def test_signin_valid_user(self):
+        """Prueba el registro de un usuario válido."""
+        response = self.client.post(reverse('signin'), {
+            'username': 'newuser',
+            'email': 'newuser@example.com',
+            'password': 'NewPass123!',
+            'user_type': 'company'
+        })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
